61aedf2b529593fedbaa97dd80cbe8b34d9d1a55
Alexander Fedotov <fedotov2a@gmail.com>
2016-02-15 13:56:39 +0300

    add canvas

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+context.font = '30px Arial';
+context.fillStyle = 'white';
+context.fillText('Здесь будет игра...', 10, 50);
