eafb5cfb57da23d388c8c77b2da6a158f4a3bb38
Alexander Fedotov <fedotov2a@gmail.com>
2016-03-06 20:17:32 +0300

    add game over

---
+++ scripts/gameOver.js
@@ -0,0 +1,11 @@
+gameOver = {
+	render: function(context) {
+		context.fillStyle="#0000bb";
+		context.fillRect(0, 0, 720, 480);
+		context.font = '40pt Calibri';
+        context.fillStyle = 'white';
+        context.fillText("КОНЕЦ ИГРЫ :(", 100, 110);
+        context.fillText("ОЧКОВ НАБРАНО: " + scoreBar.score, 100, 170);
+        context.fillText("F5 - СЫГРАТЬ ЕЩЁ РАЗ", 100, 250);
+	}
+};
