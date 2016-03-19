1ccc95cc189ea59d51e3b08735f8e284feab306c
Alexander Fedotov <fedotov2a@gmail.com>
2016-03-19 16:48:46 +0300

    fix menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,17 +6,8 @@
 * @constructor
 */
 var menu = {
-<<<<<<< HEAD
     muteOn: true,
 
-=======
-    muteOn: true,    
-    
- 
-    
-    
-    
->>>>>>> gh-pages
     authorsImage: new Image(),
     bgImage: new Image(),    
     bgImage: new Image(),
@@ -49,15 +40,11 @@ var menu = {
     * @param {CanvasRenderingContext2D} context место для рисования.
     */
     render: function(context) {
-    
         context.clearRect(0, 0, game.width, game.height);
         context.drawImage(this.bgImage, 0, 0);
         context.drawImage(this.logoImage, 170,10);
         context.drawImage(this.playImage, this.buttonX[0], this.buttonY[0]);
-<<<<<<< HEAD
 
-=======
->>>>>>> gh-pages
         if (this.isClickPlay) {
             context.drawImage(this.playClickImage, this.buttonX[0], this.buttonY[0]);
             this.isClickPlay = false;
@@ -81,17 +68,13 @@ var menu = {
     * @param {Event} mouseEvent событие мыши.
     */
     checkClick: function(mouseEvent){
-<<<<<<< HEAD
         // кнопка Играть (Play)
         // console.log(mouseEvent.offsetX + " " + mouseEvent.offsetY);
         if(mouseEvent.offsetX > menu.buttonX[0] && mouseEvent.offsetX < menu.buttonX[0] + menu.buttonWidth[0]) {
             if(mouseEvent.offsetY > menu.buttonY[0] && mouseEvent.offsetY < menu.buttonY[0] + menu.buttonHeight[0]) {
                 menu.isClickPlay = true;
-                
-                // canvas.removeEventListener("mousemove", menu.checkPos);
-                // canvas.removeEventListener("mouseup", menu.checkClick);
-               canvas.removeEventListener("mouseup", menu.checkClick);
-               game.currentState = state.GAME; // переключить состояние
+                canvas.removeEventListener("mouseup", menu.checkClick);
+                game.currentState = state.GAME;
             }
         }
         
@@ -102,9 +85,6 @@ var menu = {
                 canvas.removeEventListener('mouseup', menu.checkClick);
                 canvas.addEventListener('mouseup', rules.checkClick);
                 game.currentState = state.RULES;
-                
-                // canvas.removeEventListener("mousemove", menu.checkPos);
-                // canvas.removeEventListener("mouseup", menu.checkClick);
             }
         }
         
@@ -124,53 +104,4 @@ var menu = {
             }
         }  
     }
-=======
-        
-//           canvas.addEventListener("mousemove", checkPos);
-//	       canvas.addEventListener("mouseup", checkClick);
-        
-            // кнопка Играть (Play)
-            // console.log(mouseEvent.offsetX + " " + mouseEvent.offsetY);
-            if(mouseEvent.offsetX > menu.buttonX[0] && mouseEvent.offsetX < menu.buttonX[0] + menu.buttonWidth[0]){
-                if(mouseEvent.offsetY > menu.buttonY[0] && mouseEvent.offsetY < menu.buttonY[0] + menu.buttonHeight[0]){
-                    menu.isClickPlay = true;
-                    
-                    // canvas.removeEventListener("mousemove", menu.checkPos);
-                    // canvas.removeEventListener("mouseup", menu.checkClick);
-
-                   game.currentState = state.GAME; // переключить состояние
-                }
-            }
-            
-            // кнопка Правила (Rules)
-            if(mouseEvent.offsetX > menu.buttonX[1] && mouseEvent.offsetX < menu.buttonX[1] + menu.buttonWidth[1]){
-                if(mouseEvent.offsetY > menu.buttonY[1] && mouseEvent.offsetY < menu.buttonY[1] + menu.buttonHeight[1]){
-                    menu.isClickRules = true;
-                    console.log(menu.isClickRules);
-                    game.currentState = state.RULES;
-                    
-                    // canvas.removeEventListener("mousemove", menu.checkPos);
-                    // canvas.removeEventListener("mouseup", menu.checkClick);
-                }
-            }
-            
-            // кнопка Вкл/Откл звука (Mute On/off)
-            if(mouseEvent.offsetX > menu.buttonX[2] && mouseEvent.offsetX < menu.buttonX[2] + menu.buttonWidth[2]){
-                if(mouseEvent.offsetY > menu.buttonY[2] && mouseEvent.offsetY < menu.buttonY[2] + menu.buttonHeight[2]){
-                        if (menu.muteOn) {
-                            context.clearRect(menu.buttonX[2], menu.buttonY[2], menu.buttonWidth[3], menu.buttonHeight[3]);
-                            context.drawImage(menu.muteOffButton, menu.buttonX[2], menu.buttonY[2]);
-                            menu.muteOn = false;
-                            
-                        } else {
-                            context.clearRect(menu.buttonX[2], menu.buttonY[2], menu.buttonWidth[3], menu.buttonHeight[3]);
-                            context.drawImage(menu.muteOnButton, menu.buttonX[2], menu.buttonY[2]);
-                            menu.muteOn = true;
-                        }
-                }
-            }
-            
-            
-    },
->>>>>>> gh-pages
 };
